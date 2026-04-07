198600b7d7455690ce1948b25369df85fd6e8ab7
NKDugar_55 <naveeneducation55@gmail.com>
2026-04-07 12:02:25 +0530

    chore: remove sensitive and unnecessary files from github

--- lib/config/secrets.dart
+++
@@ -1,4 +0,0 @@
-class Secrets {
-  static const supabaseUrl     = 'https://jbkbwqprwbkqoduwghjp.supabase.co';
-  static const supabaseAnonKey = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6Impia2J3cXByd2JrcW9kdXdnaGpwIiwicm9sZSI6ImFub24iLCJpYXQiOjE3NzQwMjc5NDgsImV4cCI6MjA4OTYwMzk0OH0.znNczDxJrxVV2aLI8jcg7hBuUF1CoyDk1fMUGXr7R_E';
-}
